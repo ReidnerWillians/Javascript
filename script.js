89211c9385c984f910991ea3c062a555fd65ff73
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2025-04-07 15:12:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ var area = document.getElementById('area')
 
 function entrar(){
     var nome = prompt ("Digite seu nome")
+
+    if(nome === '' || nome === null){
+        area.innerHTML = "Clique no botão para acessar..."
+    }
     
     area.innerHTML = "Bem vindo" + ' ' + nome
     
