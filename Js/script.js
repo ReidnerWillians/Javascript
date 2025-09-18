5217439e7071bdd8b001e4d281cac2f4462e9dd6
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2025-09-18 15:32:57 -0300

    js

--- Js/script.js
+++ Js/script.js
@@ -1,33 +1,56 @@
-let area = document.getElementById('area')
+/* let area = document.getElementById('area')
 
 function entrar() {
-    let nome = prompt('Digite seu nome')
-        if (nome === '' || nome === null){
-            alert('Ops, algo deu errado, preenhca o prompt.')
-            //area.innerHTML = 'Favor preencher o prompt!'
-        }else{
-            area.innerHTML = "Bem vindo " + nome + " "
+     let nome = prompt('Digite seu nome')
+         if (nome === '' || nome === null){
+             alert('Ops, algo deu errado, preenhca o prompt.')
+             area.innerHTML = 'Favor preencher o prompt!'
+         }else{
+             area.innerHTML = "Bem vindo " + nome + " "
 
-            let btnSair = document.createElement('button')
-            btnSair.innerText = 'Sair da Conta'
-            btnSair.onclick = sair
+             let btnSair = document.createElement('button')
+             btnSair.innerText = 'Sair da Conta'
+             btnSair.onclick = sair
 
-            area.appendChild(btnSair)
-        }    
-}
+             area.appendChild(btnSair)
+         }    
+ }
+ function sair() {
+     alert('Até mais!')
+     area.innerHTML = 'Você saiu da área'
+ }
 
-function sair() {
-    alert('Até mais!')
-    area.innerHTML = 'Você saiu da área'
-}
-
-function mediaAluno(nota1, nota2){
-    let media = (nota1 + nota2) / 2
+ function mediaAluno(nota1, nota2){
+     let media = (nota1 + nota2) / 2
 
     if (media >= 7){
      console.log('Aprovado com média ' + media)
     } else if (media <7) {
         console.log('Reprovado com média ' + media)
-    } 
+     } 
+ }
+     mediaAluno(3, 9)
+
+
+ function aluno(nome, curso){
+    var mensagem = "Seja bem vindo " + nome + " ao curso de " + curso
+        console.log(mensagem)
+    var times = ['Palmeiras', 'Corinthians', 'Santos']
+
+ times.push('Sao Paulo', 'Flamengo', 'Vila No
+ console.log(times)*/
+
+
+/*var x = 0
+
+while (x < 10){
+    document.writeln('O valor de x é ' + x + '<br>')
+    x++    
+}*/ 
+
+
+let valor = 15
+for (var x = 0; x <= valor; x++){
+    document.writeln('O valor de x é ' + x + '<br>')
 }
-    mediaAluno(8, 9)
+
